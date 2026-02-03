faf8b1207fbea4ec8aef149d3ee95254ec32f143
Miaritsoa2424 <tsoamiari24@gmail.com>
2026-02-03 11:38:17 +0300

    "Rest API: Get : /signalements/{id}/images"

---
+++ src/main/java/com/route/controllers/ImageController.java
@@ -0,0 +1,29 @@
+package com.route.controllers;
+
+import com.route.models.Image;
+import com.route.models.Signalement;
+import com.route.repositories.ImageRepository;
+import com.route.repositories.SignalementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ImageController {
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Autowired
+    private SignalementRepository signalementRepository;
+
+    @GetMapping("/signalements/{id}/images")
+    public List<Image> getImagesBySignalement(@PathVariable("id") Integer id) {
+        Signalement signalement = signalementRepository.findById(id).orElse(null);
+        if (signalement == null) {
+            return List.of(); // ou gérer l'erreur 404
+        }
+        return imageRepository.findBySignalement(signalement);
+    }
+}
