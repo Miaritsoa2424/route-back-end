805aed3019dc7e396f0c0e03a0b23d3f657dbf40
Miaritsoa2424 <tsoamiari24@gmail.com>
2026-02-03 11:38:05 +0300

    Fonction getImages by signalements

--- src/main/java/com/route/repositories/ImageRepository.java
+++ src/main/java/com/route/repositories/ImageRepository.java
@@ -1,11 +1,15 @@
 package com.route.repositories;
 
 import com.route.models.Image;
-
+import com.route.models.Signalement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Integer>{
-    
+    public boolean existsByIdFirestore(String idFirestore);
+    public boolean existsByLien(String lien);
+
+    List<Image> findBySignalement(Signalement signalement);
 }
