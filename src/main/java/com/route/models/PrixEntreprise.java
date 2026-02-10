71e4ec4dae5acd642eb86e6ad388f97f35fddb0b
Miaritsoa2424 <tsoamiari24@gmail.com>
2026-02-10 11:16:00 +0300

    Creation modele prix

---
+++ src/main/java/com/route/models/PrixEntreprise.java
@@ -0,0 +1,31 @@
+package com.route.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "prix_entreprise")
+public class PrixEntreprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "prix")
+    private Double prix;
+
+    @Column(name = "date_prix")
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "id_entreprise",nullable = false)
+    private Entreprise entreprise;
+    
+}
